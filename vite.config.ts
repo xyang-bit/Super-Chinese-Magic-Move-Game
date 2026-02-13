b02078e724378e0d2ae464d90e673f54f4979d68
Xi Yang <xyang@XYANG-2.local>
2026-02-12 14:45:09 -1000

    UI Fix: Full width layout

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: '/Super-Chinese-Magic-Move-Game/', // Updated to match your repo name exactly
+    base: './',
 })
